1762f15d707cb6496479e8ef28b54a946a19b2d8
Jaime Mas <sanmasja@gmail.com>
2021-02-08 10:28:45 +0100

    Aplicación de Gifs terminada

--- 02-gifsApp/src/app/gifs/services/gifs.service.ts
+++ 02-gifsApp/src/app/gifs/services/gifs.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Gif, SearchGifsResponse } from '../interface/gifs.interface';
 
 @Injectable({
@@ -7,33 +7,45 @@ import { Gif, SearchGifsResponse } from '../interface/gifs.interface';
 })
 export class GifsService {
 
-  private _apiKey   : string = 'mi4TgJTu2nYNPKNUuXcMr0129ydVNJ8w';
-  private _historial: string[] = [];
+  private _apiKey    : string = 'mi4TgJTu2nYNPKNUuXcMr0129ydVNJ8w';
+  private servicioUrl: string = 'https://api.giphy.com/v1/gifs';
+  private _historial : string[] = [];
 
-  public resultados: Gif[] = [];
+  public resultados  : Gif[] = [];
   
   get historial() {
     return [...this._historial];
   }
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+    this._historial = JSON.parse(localStorage.getItem('historial')!) || []; 
+    this.resultados = JSON.parse(localStorage.getItem('resultados')!) || []; 
+  }
 
-  buscarGifs(query: string) {
+  buscarGifs(query: string = '') {
 
     query = query.trim().toLocaleLowerCase();
 
     if(this._historial.includes(query)) {
       const index: number = this._historial.indexOf(query);
       this._historial.splice(index, 1);
-    } 
+
+    }
 
     this._historial.unshift(query);
     this._historial = this._historial.splice(0, 10);
+
+    localStorage.setItem('historial', JSON.stringify(this._historial));
     
-    this.http.get<SearchGifsResponse>(`https://api.giphy.com/v1/gifs/search?api_key=mi4TgJTu2nYNPKNUuXcMr0129ydVNJ8w&q=${query}&limit=10`)
+    const params = new HttpParams()
+        .set('api_key', this._apiKey)
+        .set('limit', '10')
+        .set('q', query);
+
+    this.http.get<SearchGifsResponse>(`${this.servicioUrl}/search`, {params})
         .subscribe((resp) => {
-          console.log(resp.data)
           this.resultados = resp.data;
+           localStorage.setItem('resultados', JSON.stringify(this.resultados));
         });
 
   }
--- 02-gifsApp/src/app/shared/sidebar/sidebar.component.ts
+++ 02-gifsApp/src/app/shared/sidebar/sidebar.component.ts
@@ -1,19 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { GifsService } from '../../gifs/services/gifs.service';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html'
 })
-export class SidebarComponent implements OnInit {
+export class SidebarComponent {
 
-  constructor(private gifsService: GifsService) {}
+  
 
   get historial() {
     return this.gifsService.historial;
   }
 
-  ngOnInit(): void {
+  constructor(private gifsService: GifsService) {}
+
+  buscar(termino: string) {
+    this.gifsService.buscarGifs(termino);
   }
 
 }
